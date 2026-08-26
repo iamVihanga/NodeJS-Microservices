0ede6fe9a6eb18b618e4c5ff4afb719f9ee81b84
iamVihanga <vihangarashansilva@gmail.com>
2026-08-26 22:32:21 +0530

    feat: add API Gateway service with proxy and rate limiting

---
+++ services/api-gateway/src/index.ts
@@ -0,0 +1,69 @@
+import { config } from "dotenv";
+import { resolve } from "node:path";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit, { MINUTE } from "express-rate-limit";
+import {
+  AppError,
+  errorHandler,
+  httpLogger,
+  logger,
+  successResponse
+} from "shared";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+// Dotenv Configurations
+config({ path: resolve(process.cwd(), ".env") });
+config({ path: resolve(process.cwd(), "../../.env") });
+
+// Constants
+const PORT = process.env.PORT || 3000; // This will be the default API Gateway PORT
+const AUTH_SERVICE_URL =
+  process.env.AUTH_SERVICE_URL || "http://localhost:3001";
+
+// Server
+const app = express();
+
+app.use(helmet()); // secure default http headers
+app.use(cors());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: true,
+    legacyHeaders: false // Disable the `X-RateLimit-*` headers.
+  })
+);
+app.use(httpLogger);
+
+// Healthcheck
+app.use("/health", (_req, res) => {
+  successResponse(res, { service: "api-gateway" });
+});
+
+// Proxy routes
+
+// 1. Auth Proxy
+// -- Forward anything from "http://localhost:3000/auth/*" -> "http://localhost:3001/api/*"
+app.use(
+  "/auth",
+  createProxyMiddleware({
+    target: AUTH_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: (path) => `/auth${path}`
+  })
+);
+
+// Error Catching
+app.use((_req, res, next) => {
+  next(new AppError(404, "Route not found"));
+});
+
+app.use(errorHandler);
+
+// ---
+
+app.listen(PORT, () => {
+  logger.info(`[API Gateway Service] has started on ${PORT}`);
+});
